9a32c5d609012c8bc3abf57da65249c01c81a80c
MatiasV4squez <mvasquez2023@alu.uct.cl>
2026-09-09 12:23:18 -0300

    Initial push; creacion de la base del proyecto (apartir de ahora la main es APP deben crear otras ramas con feature/[la tarea])

---
+++ proyect-app-studia/src/app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Studia</ThemedText>
+      <ThemedText type="small" themeColor="textSecondary">
+        Edit src/app/index.tsx to edit this screen.
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 8,
+  },
+});
